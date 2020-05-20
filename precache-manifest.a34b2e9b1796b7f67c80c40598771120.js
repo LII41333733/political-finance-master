5a191938ff41f2bade12dea57ee90a5566d9ef4e
Mark Fisher <mfisher36@gmail.com>
2020-05-19 22:03:17 -0400

    Updates

---
+++ precache-manifest.a34b2e9b1796b7f67c80c40598771120.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f370e957120cebe7c3e131130c48950",
+    "url": "/political-finance-master/index.html"
+  },
+  {
+    "revision": "6d4f169d1b642cb8750b",
+    "url": "/political-finance-master/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "e083673a97349bb427ad",
+    "url": "/political-finance-master/static/css/main.2ee7e509.chunk.css"
+  },
+  {
+    "revision": "6d4f169d1b642cb8750b",
+    "url": "/political-finance-master/static/js/2.9a3ec843.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/political-finance-master/static/js/2.9a3ec843.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e083673a97349bb427ad",
+    "url": "/political-finance-master/static/js/main.7e2a9cb7.chunk.js"
+  },
+  {
+    "revision": "20550ac20ada7ecccb31",
+    "url": "/political-finance-master/static/js/runtime-main.3edad1d0.js"
+  },
+  {
+    "revision": "55bb96d3a46cee9e36d69c21689206a0",
+    "url": "/political-finance-master/static/media/SchadowBlackCondensed.55bb96d3.ttf"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/political-finance-master/static/media/logo.5d5d9eef.svg"
+  }
+]);
